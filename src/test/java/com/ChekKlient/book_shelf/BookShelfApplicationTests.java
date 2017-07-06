07539281ea96b7c05678cbb19c589f18d427303b
ChekKlient <www2000@ukr.net>
2017-07-06 13:03:19 +0300

    First

---
+++ src/test/java/com/ChekKlient/book_shelf/BookShelfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ChekKlient.book_shelf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookShelfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
