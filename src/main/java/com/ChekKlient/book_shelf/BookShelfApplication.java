06d2720013f57922ade63563ba4494e5b8a87159
ChekKlient <www2000@ukr.net>
2017-07-12 10:31:32 +0300

    Add Genre, Genre service and UI

--- src/main/java/com/ChekKlient/book_shelf/BookShelfApplication.java
+++ src/main/java/com/ChekKlient/book_shelf/BookShelfApplication.java
@@ -1,5 +1,7 @@
 package com.ChekKlient.book_shelf;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
